a324ebe02ace1ba57869578fe84f2425ff31d8e7
Rochelle Fernandes <149226541+Rochelle-Fernandes@users.noreply.github.com>
2025-07-31 10:59:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from sjf_algo import sjf_scheduler
-from gannt_chart import print_gantt_chart
 from results import print_results
 
 orders = [          # Input orders
@@ -9,7 +8,5 @@ orders = [          # Input orders
 ]
 
 result = sjf_scheduler(orders)
-
-print_gantt_chart(result["schedule"], [o[2] for o in orders])
 
 print_results(orders, result)
